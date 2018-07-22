b3b54bceae5ac8bef38e480c2fa1518da04169cc
Somsubhra Das <das.somsubhra1@gmail.com>
2018-07-22 22:22:45 +0530

    Added new features to the script

--- git.sh
+++ git.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -p "Project name: " name
 read -p "Full path of project: " -e path
+read -p "commit message: " msg
 
-git clone --bare $path $name.git
-
-cp -Rf $path/.git $name.git
-
-echo "Completed. Check directory from where you ran this script to get the .git file."
+cd $path
+git init
+git add .
+git commit -m "$msg"
+echo "Completed."
