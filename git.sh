9a8a801edd217d2c6554c133be876b9787f1897b
Somsubhra Das <das.somsubhra1@gmail.com>
2018-07-29 21:00:08 +0530

    Feature to add updated or new files to pre existing repository

--- git.sh
+++ git.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+read -p "Enter 1: Initiate new repository || 2: Commit / add new files to existing repository: " choice
 read -p "Full path of project: " -e path
 read -p "commit message: " msg
 
 cd $path
-git init
+
+if [[ $choice == 1 ]]
+then
+    git init
+fi
+
 git add .
 git commit -m "$msg"
 echo "Completed."
